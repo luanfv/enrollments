e6efda5bf5644621cb1905d41450df34153408dc
luanfv <luanfrancavieira@gmail.com>
2026-03-16 15:27:02 -0300

    feat: add class validator

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 declare const process: { env?: { PORT?: string } };
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env?.PORT ?? 3000);
 }
 bootstrap();
--- src/members/infra/controllers/dtos/create-member.dto.ts
+++ src/members/infra/controllers/dtos/create-member.dto.ts
@@ -1,6 +1,18 @@
+import { IsNotEmpty, Matches } from 'class-validator';
+
 export class CreateMemberDto {
+  @IsNotEmpty({ message: 'name cannot be empty' })
   name: string;
+
+  @IsNotEmpty({ message: 'email cannot be empty' })
   email: string;
+
+  @IsNotEmpty({ message: 'birthdate cannot be empty' })
+  @Matches(/^\d{2}\/\d{2}\/\d{4}$/, {
+    message: 'birthdate must be in DD/MM/YYYY format',
+  })
   birthdate: string;
+
+  @IsNotEmpty({ message: 'gender cannot be empty' })
   gender: string;
 }
